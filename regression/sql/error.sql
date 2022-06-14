a4a31e7a436e9b75410991acc0c24e366c7f3fc4
Ibrar Ahmed <ibrar.ahmed@percona.com>
2022-06-14 16:18:33 +0000

    PG-440: Regression Cleanup.
    In this commit, we removed unnecessary files which no longer in use, along
    with combining similar test cases in a single file to avoid duplication.

--- regression/sql/error.sql
+++ regression/sql/error.sql
@@ -11,4 +11,20 @@ END $$;
 
 SELECT query, elevel, sqlcode, message FROM pg_stat_monitor ORDER BY query COLLATE "C",elevel;
 SELECT pg_stat_monitor_reset();
+
+DROP TABLE IF EXISTS Company;
+
+CREATE TABLE Company(
+   ID INT PRIMARY KEY     NOT NULL,
+   NAME TEXT    NOT NULL
+);
+
+SELECT pg_stat_monitor_reset();
+INSERT  INTO Company(ID, Name) VALUES (1, 'Percona');
+INSERT  INTO Company(ID, Name) VALUES (1, 'Percona');
+
+DROP TABLE IF EXISTS Company;
+SELECT query, elevel, sqlcode, message FROM pg_stat_monitor ORDER BY query COLLATE "C",elevel;
+SELECT pg_stat_monitor_reset();
 DROP EXTENSION pg_stat_monitor;
+
