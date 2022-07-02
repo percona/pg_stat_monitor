aa9aaf55305256f5276efc07cc09ad3dae34b475
Naeem Akhter <naeem.akhter@percona.com>
2022-07-03 02:49:07 +0500

    PG-451: Fix regression failures in main branch.
    Regression was failing in main branch after merging REL_1_STABLE. Changes to
    stablise the regression are summarised below.
    
    Updated following TAP testcase with 'where' clause for filtering GUC specific row
    from settings view. Intention is to fetch only GUC value and test it.
    1) t/001_settings_default.pl
    2) t/002_settings_pgsm_track_planning.pl
    3) t/003_settings_pgms_extract_comments.pl
    4) t/004_settings_pgsm_track.pl
    5) t/005_settings_pgsm_enable_query_plan.pl
    6) t/006_settings_pgsm_overflow_target.pl
    7) t/007_settings_pgsm_query_shared_buffer.pl
    8) t/008_settings_pgsm_histogram_buckets.pl
    9) t/009_settings_pgsm_histogram_max.pl
    10) t/010_settings_pgsm_histogram_min.pl
    11) t/011_settings_pgsm_bucket_time.pl
    12) t/012_settings_pgsm_max_buckets.pl
    13) t/013_settings_pgsm_normalized_query.pl
    14) t/014_settings_pgsm_track_utility.pl
    15) t/015_settings_pgsm_query_max_len.pl
    16) t/016_settings_pgsm_max.pl
    
    Updated following expected files for TAP testcase where testcase was updated
    with 'where' clause to filter GUC specific row only from settings view.
    1) t/expected/001_settings_default.out
    2) t/expected/002_settings_pgsm_track.outanning.out
    3) t/expected/003_settings_pgms_extract_comments.out
    4) t/expected/004_settings_pgsm_track.out
    5) t/expected/005_settings_pgsm_enable_query.out
    6) t/expected/006_settings_pgsm_overflow_target.out
    7) t/expected/007_settings_pgsm_query_shared_buffer.out
    8) t/expected/008_settings_pgsm_histogram_buckets.out
    9) t/expected/009_settings_pgsm_histogram_max.out
    10) t/expected/010_settings_pgsm_histogram_min.out
    11) t/expected/011_settings_pgsm_bucket_time.out
    12) t/expected/012_settings_pgsm_max_buckets.out
    13) t/expected/013_settings_pgsm_normalized_query.out
    14) t/expected/014_settings_pgsm_track_utility.out
    15) t/expected/015_settings_pgsm_query_max_len.out
    16) t/expected/016_settings_pgsm_max.out
    17) t/expected/017_execution_stats.out
    
    Updated expected files for following sql based testcases due to changed default
    value of pgsm_normalized_query.
    1) error_insert_1.out
    2) guc_1.out
    3) top_query_1.out
    
    Enabled GUC pgsm_normalized_query to 'yes' for following testcases.
    1) t/024_check_timings.pl
    2) t/025_compare_pgss.pl
    3) t/026_shared_blocks.pl
    
    Added the output file for histogram testcase to accomodate behaviour.
    
    Changes suggested by Ibrar to make regression more modular (PG-440), are also
    made part of regression so those these can used for future improvements.
    
    Reduce scale for pgBench load from 100 to 10, trasaction from 1000 to 100 to
    decrease execution time in t/007_settings_pgsm_query_shared_buffer.pl.
    
    Removed commented/unneeded steps from t/017_execution_stats.pl.
    
    Updated testcase tags.sql and output file tags.out to accomodate enabling of
    pgsm_extract_comments to yes.
    
    Updated error_1.out output file with required output, it was overwritten in
    one of previous commits.
    
    Updated test condition (count of calls and bucket) in t/023_missing_queries.pl
    as per comment from Ibrar in PG-461 where PGSM does not track the pg_sleep in
    some cases.

--- t/023_missing_queries.pl
+++ t/023_missing_queries.pl
@@ -37,45 +37,6 @@ use File::Compare;
 use PostgresNode;
 use Test::More;
 
-# Expected folder where expected output will be present
-my $expected_folder = "t/expected";
-
-# Results/out folder where generated results files will be placed
-my $results_folder = "t/results";
-
-# Check if results folder exists or not, create if it doesn't
-unless (-d $results_folder)
-{
-   mkdir $results_folder or die "Can't create folder $results_folder: $!\n";;
-}
-
-# Check if expected folder exists or not, bail out if it doesn't
-unless (-d $expected_folder)
-{
-   BAIL_OUT "Expected files folder $expected_folder doesn't exist: \n";;
-}
-
-# Get filename of the this perl file
-my $perlfilename = basename($0);
-
-#Remove .pl from filename and store in a variable
-$perlfilename =~ s/\.[^.]+$//;
-my $filename_without_extension = $perlfilename;
-
-# Create expected filename with path
-my $expected_filename = "${filename_without_extension}.out";
-my $expected_filename_with_path = "${expected_folder}/${expected_filename}" ;
-
-# Create results filename with path
-my $out_filename = "${filename_without_extension}.out";
-my $out_filename_with_path = "${results_folder}/${out_filename}" ;
-
-# Delete already existing result out file, if it exists.
-if ( -f $out_filename_with_path)
-{
-   unlink($out_filename_with_path) or die "Can't delete already existing $out_filename_with_path: $!\n";
-}
-
 # Create new PostgreSQL node and do initdb
 my $node = PostgresNode->get_new_node('test');
 my $pgdata = $node->data_dir;
@@ -91,31 +52,21 @@ ok($rt_value == 1, "Start Server");
 
 my ($cmdret, $stdout, $stderr) = $node->psql('postgres', 'CREATE EXTENSION pg_stat_monitor;', extra_params => ['-a']);
 ok($cmdret == 0, "Create PGSM Extension");
-TestLib::append_to_file($out_filename_with_path, $stdout . "\n");
-
-($cmdret, $stdout, $stderr) = $node->psql('postgres', "SELECT * from pg_stat_monitor_settings;", extra_params => ['-a', '-Pformat=aligned','-Ptuples_only=off']);
-ok($cmdret == 0, "Print PGSM Extension Settings");
-TestLib::append_to_file($out_filename_with_path, $stdout . "\n");
 
 ($cmdret, $stdout, $stderr) = $node->psql('postgres', 'SELECT pg_stat_monitor_reset();', extra_params => ['-a', '-Pformat=aligned','-Ptuples_only=off']);
 ok($cmdret == 0, "Reset PGSM Extension");
-TestLib::append_to_file($out_filename_with_path, $stdout . "\n");
 
 ($cmdret, $stdout, $stderr) = $node->psql('postgres', "SELECT pg_sleep(5)");
 ok($cmdret == 0, "1 - SELECT pg_sleep(5)");
-TestLib::append_to_file($out_filename_with_path, $stdout . "\n");
 
 ($cmdret, $stdout, $stderr) = $node->psql('postgres', "SELECT pg_sleep(5)");
 ok($cmdret == 0, "2 - SELECT pg_sleep(5)");
-TestLib::append_to_file($out_filename_with_path, $stdout . "\n");
 
 ($cmdret, $stdout, $stderr) = $node->psql('postgres', "SELECT pg_sleep(5)");
 ok($cmdret == 0, "3 - SELECT pg_sleep(5)");
-TestLib::append_to_file($out_filename_with_path, $stdout . "\n");
 
 ($cmdret, $stdout, $stderr) = $node->psql('postgres', 'SELECT bucket, queryid, calls, query FROM pg_stat_monitor;', extra_params => ['-a', '-Pformat=aligned']);
 ok($cmdret == 0, "Query pg_stat_monitor view");
-TestLib::append_to_file($out_filename_with_path, $stdout . "\n");
 
 sub trim { my $s = shift; $s =~ s/^\s+|\s+$//g; return $s };
 
@@ -132,10 +83,6 @@ foreach my $line(@lines) {
     my $ncalls = trim($tokens[2]);
     my $query = trim($tokens[3]);
 
-    TestLib::append_to_file($out_filename_with_path, "calls: " . $calls . "\n");
-    TestLib::append_to_file($out_filename_with_path, "bucket_cnt: " . $bucket_cnt . "\n");
-    TestLib::append_to_file($out_filename_with_path, "--------------------------------- \n");
-
     if ($query =~ "SELECT pg_sleep") {
         $calls += $ncalls;
         if ($bucket_cnt == 0) {
@@ -148,9 +95,6 @@ foreach my $line(@lines) {
     }
 }
 
-TestLib::append_to_file($out_filename_with_path, "calls: " . $calls . "\n");
-TestLib::append_to_file($out_filename_with_path, "bucket_cnt: " . $bucket_cnt . "\n");
-
 ok($calls == 3 || $calls == 2, "Check total query count is correct");
 ok($bucket_cnt == 2 || $bucket_cnt == 1, "Check total bucket count is correct");
 
@@ -158,5 +102,3 @@ ok($bucket_cnt == 2 || $bucket_cnt == 1, "Check total bucket count is correct");
 $node->stop;
 # Done testing for this testcase file.
 done_testing();
-
-
