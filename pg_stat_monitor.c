72866090d4cc8571577c51577c3d5e28398afc43
Artem Gavrilov <artem.gavrilov@percona.com>
2024-07-03 13:00:36 +0200

    Fix

--- pg_stat_monitor.c
+++ pg_stat_monitor.c
@@ -1261,7 +1261,7 @@ pg_get_backend_status(void)
 
 #if PG_VERSION_NUM >= 170000
 	return &(pgstat_get_beentry_by_proc_number(MyProcPid)->backendStatus);
-#elif PG_VERSION_NUL >= 160000
+#elif PG_VERSION_NUM >= 160000
 	return &(pgstat_get_local_beentry_by_backend_id(MyBackendId)->backendStatus);
 #else
 	LocalPgBackendStatus *local_beentry;
