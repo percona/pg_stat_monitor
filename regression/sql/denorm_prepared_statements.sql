31338513335a7aa8aa5045e1239750f464a57d47
Diego Fronza <darkfronza@gmail.com>
2024-08-09 16:23:47 -0300

    Add query denormalization regression test for prepared statements
    Ensures that the denormalization of prepared statements is working, also
    ensure that a query which takes more time to execute replaces the
    previous denormalized query.

---
+++ regression/sql/denorm_prepared_statements.sql
@@ -0,0 +1,19 @@
+CREATE EXTENSION pg_stat_monitor;
+Set pg_stat_monitor.pgsm_normalized_query='off';
+
+CREATE TABLE t1 (a TEXT, b TEXT, c TEXT);
+
+SELECT pg_stat_monitor_reset();
+
+PREPARE prepstmt(TEXT, TEXT, TEXT) AS INSERT INTO t1(a, b, c) VALUES($1, $2, $3);
+
+EXECUTE prepstmt('A', 'B', 'C');
+SELECT SUBSTRING(query, 0, 128), calls FROM pg_stat_monitor ORDER BY query COLLATE "C";
+
+EXECUTE prepstmt(REPEAT('XYZ', 8192), md5(random()::text), REPEAT('RANDOM', 4096));
+SELECT SUBSTRING(query, 0, 128), calls FROM pg_stat_monitor ORDER BY query COLLATE "C";
+
+DROP TABLE t1;
+
+SELECT pg_stat_monitor_reset();
+DROP EXTENSION pg_stat_monitor;
