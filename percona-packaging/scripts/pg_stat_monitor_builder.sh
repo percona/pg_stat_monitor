8623d8c73fcd708ea0226366d8c7771f63d223ed
Muhammad Aqeel <muhammad.aqeel@percona.com>
2024-11-22 12:24:54 +0500

    Fixes llvm-devel package installation issue.

--- percona-packaging/scripts/pg_stat_monitor_builder.sh
+++ percona-packaging/scripts/pg_stat_monitor_builder.sh
@@ -211,17 +211,18 @@ install_deps() {
 
         if [ x"$RHEL" = x8 ];
         then
-                clang_version=$(yum list --showduplicates clang-devel | grep "17.0" | grep clang | awk '{print $2}' | head -n 1)
-                yum install -y clang-devel-${clang_version} clang-${clang_version}
+		clang_version=$(yum list --showduplicates clang-devel | grep "17.0" | grep clang | awk '{print $2}' | head -n 1)
+                llvm_version=$(yum list --showduplicates llvm-devel | grep "17.0" | grep llvm | awk '{print $2}' | head -n 1)
+                yum install -y clang-devel-${clang_version} clang-${clang_version} llvm-devel-${llvm_version}
                 dnf module -y disable llvm-toolset
         else
-                yum install -y clang-devel clang
+                yum install -y clang-devel clang llvm-devel
         fi
 
         PKGLIST="percona-postgresql${PG_RELEASE}-devel"
-        PKGLIST+=" git llvm-devel rpmdevtools vim wget"
+        PKGLIST+=" git rpmdevtools vim wget"
         PKGLIST+=" perl binutils gcc gcc-c++"
-        PKGLIST+=" llvm-devel git rpm-build rpmdevtools wget gcc make autoconf"
+        PKGLIST+=" git rpm-build rpmdevtools wget gcc make autoconf"
         if [[ "${RHEL}" -ge 8 ]]; then
             dnf config-manager --set-enabled ol${RHEL}_codeready_builder
             dnf -y module disable postgresql || true
