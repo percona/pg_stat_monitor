61662cc58ff12792855dc9b163e48c504481f881
Zsolt Parragi <zsolt.parragi@percona.com>
2025-06-17 15:52:28 +0200

    PG-1621: fix cmd_type mostly showing 0 values (#538)
    This was actually caused by two bugs internally:
    
    * cmd_type was only set in some codepaths, other parts of the code
    never set a value. Depending on which query / how was executed,
    it was possibly never changed (after a reset to 0)
    * the update first set the cmd_type, then reset all counters. As
    the cmd_type is stored within the counters for some reason, this
    reset its value to 0 in most execution paths, even if it was corretly
    set before.
    
    And according to this the fix is simple:
    
    * cmd_type is now set in all codepaths except for failing queries,
    as we only have the error string in this case, without the type.
    * in the update logic, we again overwrite cmd_type with the proper
    value after a reset

---
+++ pg_stat_monitor--2.1--2.2.sql
@@ -0,0 +1,21 @@
+/* contrib/pg_stat_monitor/pg_stat_monitor--2.1--2.2.sql */
+
+-- complain if script is sourced in psql, rather than via CREATE EXTENSION
+\echo Use "ALTER EXTENSION pg_stat_monitor" to load this file. \quit
+
+CREATE OR REPLACE FUNCTION get_cmd_type (cmd_type INTEGER) RETURNS TEXT AS
+$$
+SELECT
+    CASE
+        WHEN cmd_type = 1 THEN 'SELECT'
+        WHEN cmd_type = 2 THEN 'UPDATE'
+        WHEN cmd_type = 3 THEN 'INSERT'
+        WHEN cmd_type = 4 THEN 'DELETE'
+        WHEN cmd_type = 5 AND current_setting('server_version_num')::int >= 150000 THEN 'MERGE'
+        WHEN cmd_type = 5 AND current_setting('server_version_num')::int < 150000 THEN 'UTILITY'
+        WHEN cmd_type = 6 AND current_setting('server_version_num')::int >= 150000 THEN 'UTILITY'
+        WHEN cmd_type = 6 AND current_setting('server_version_num')::int < 150000 THEN 'NOTHING'
+        WHEN cmd_type = 7 THEN 'NOTHING'
+    END
+$$
+LANGUAGE SQL PARALLEL SAFE;
