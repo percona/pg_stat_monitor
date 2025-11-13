d2c1842fde62b50a11e9399778b498b1549e3fa9
Artem Gavrilov <artem.gavrilov@percona.com>
2025-11-13 15:29:50 +0200

    Bump version to 2.3.1
    Increment pg_stat_monitor version to 2.3.1

--- pg_stat_monitor.c
+++ pg_stat_monitor.c
@@ -43,7 +43,7 @@ typedef enum pgsmVersion
 
 PG_MODULE_MAGIC;
 
-#define BUILD_VERSION                   "2.3.0"
+#define BUILD_VERSION                   "2.3.1"
 
 /* Number of output arguments (columns) for various API versions */
 #define PG_STAT_MONITOR_COLS_V1_0    52
