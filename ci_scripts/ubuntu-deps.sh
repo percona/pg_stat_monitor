a8a81838b68b11e0632e921e1a3c49af5e3b3efb
Artem Gavrilov <artem.gavrilov@percona.com>
2026-02-13 21:00:24 +0200

    Fix

--- ci_scripts/ubuntu-deps.sh
+++ ci_scripts/ubuntu-deps.sh
@@ -16,5 +16,43 @@ DEPS=(
     perltidy
 )
 
+case "$1" in
+    dev)
+        DEPS+=(
+            # Build
+            bison
+            docbook-xml
+            docbook-xsl
+            flex
+            gettext
+            libicu-dev
+            libkrb5-dev
+            libldap2-dev
+            liblz4-dev
+            libpam0g-dev
+            libperl-dev
+            libreadline-dev
+            libselinux1-dev
+            libssl-dev
+            libsystemd-dev
+            liburing-dev
+            libxml2-dev
+            libxml2-utils
+            libxslt1-dev
+            libzstd-dev
+            lz4
+            mawk
+            perl
+            pkgconf
+            systemtap-sdt-dev
+            tcl-dev
+            uuid-dev
+            xsltproc
+            zlib1g-dev
+            zstd
+            )
+        ;;
+esac
+
 sudo apt-get update
 sudo apt-get install -y ${DEPS[@]}
