383af12d9085eefcfc32faec0715d96e0945ff7a
Artem Gavrilov <artem.gavrilov@percona.com>
2026-03-10 18:12:49 +0200

    Delete state regression test
    There is no such field since v1. This file is leftover.

--- regression/sql/state.sql
+++
@@ -1,8 +0,0 @@
-CREATE EXTENSION pg_stat_monitor;
-SELECT pg_stat_monitor_reset();
-SELECT 1;
-SELECT 1/0;   -- divide by zero
-
-SELECT query, state_code, state FROM pg_stat_monitor ORDER BY query COLLATE "C";
-SELECT pg_stat_monitor_reset();
-DROP EXTENSION pg_stat_monitor;
