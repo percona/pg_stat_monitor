2e9f2c361ce3a3f9ce8a5abc7f088850890a23a3
Artem Gavrilov <artem.gavrilov@percona.com>
2026-03-10 19:03:34 +0200

    Fix and restore histogram regression test
    For some reason this test was disabled since PGSM v1. This commit brings
    it back with some improvements. This test was flacky because data it
    produces was distributed in buckets randomly, that leads to multiple
    test output variations. This commit modifies test so that waits for new
    bucket start if there is not enough time left in the current bucket to
    fit required test data.

--- regression/sql/histogram.sql
+++ regression/sql/histogram.sql
@@ -1,16 +1,15 @@
 CREATE OR REPLACE FUNCTION generate_histogram()
     RETURNS TABLE (
-    range TEXT, freq INT, bar TEXT
+    RANGE TEXT, freq INT, bar TEXT
   )  AS $$
-Declare
+DECLARE
     bucket_id integer;
-    query_id text;
+    query_id int8;
 BEGIN
-    select bucket into bucket_id from pg_stat_monitor order by calls desc limit 1;
-    select queryid into query_id from pg_stat_monitor order by calls desc limit 1;
-    --RAISE INFO 'bucket_id %', bucket_id;
-    --RAISE INFO 'query_id %', query_id;
-    return query
+    SELECT bucket, queryid INTO bucket_id, query_id FROM pg_stat_monitor ORDER BY calls DESC LIMIT 1;
+    -- RAISE INFO 'bucket_id %', bucket_id;
+    -- RAISE INFO 'query_id %', query_id;
+    RETURN query
     SELECT * FROM histogram(bucket_id, query_id) AS a(range TEXT, freq INT, bar TEXT);
 END;
 $$ LANGUAGE plpgsql;
@@ -20,20 +19,41 @@ DECLARE
     loops ALIAS FOR $1;
 BEGIN
     FOR i IN 1..loops LOOP
-	--RAISE INFO 'Current timestamp: %', timeofday()::TIMESTAMP;
-	RAISE INFO 'Sleep % seconds', i;
-	PERFORM pg_sleep(i);
+	-- RAISE INFO 'Current timestamp: %', timeofday()::TIMESTAMP;
+    -- 0.4 seconds step used here to hit the same histogram buckets consistently.
+    -- See histogram buckets timing distribution.
+	PERFORM pg_sleep(0.4 * i);
     END LOOP;
 END;
 $$ LANGUAGE 'plpgsql' STRICT;
 
+CREATE OR REPLACE FUNCTION wait_for_new_bucket() RETURNS VOID AS $$
+DECLARE
+    bucket_left_time int;
+BEGIN
+    -- This test works correctly only if the bucket duration is default (60 seconds).
+    SELECT 60 - EXTRACT(
+        SECOND FROM(
+            SELECT CURRENT_TIMESTAMP(0) bucket_start_time FROM pg_stat_monitor ORDER BY bucket_start_time DESC LIMIT 1
+        )
+    )::int INTO bucket_left_time; 
+
+    -- If the bucket lifetime is less than 10 seconds, we would not fit.
+    IF bucket_left_time <= 4 THEN
+        -- RAISE NOTICE 'Bucket lifetime comes to an end → sleeping 4 seconds...';
+        PERFORM pg_sleep(4);
+    END IF;
+END;
+$$ LANGUAGE plpgsql;
+
 CREATE EXTENSION pg_stat_monitor;
+SELECT wait_for_new_bucket();
 SELECT pg_stat_monitor_reset();
-Set pg_stat_monitor.pgsm_track='all';
-select run_pg_sleep(5);
+SET pg_stat_monitor.pgsm_track='all';
+SELECT run_pg_sleep(5);
 
-SELECT substr(query, 0,50) as query, calls, resp_calls FROM pg_stat_monitor ORDER BY query COLLATE "C";
+SELECT substr(query, 0, 50) as query, calls, resp_calls FROM pg_stat_monitor ORDER BY query COLLATE "C";
 
-select * from generate_histogram();
+SELECT * FROM generate_histogram();
 
 DROP EXTENSION pg_stat_monitor;
