1e111666cb2d8ee0edbf038fea8d7330d30c719e
Andreas Karlsson <andreas.karlsson@percona.com>
2026-04-07 09:32:35 +0200

    Remove PostgreSQL 13 support from decode_error_level()
    Commit 3481ee75eba8e333d402e2bf2511823f950eefee removed PostgreSQL 13
    support but missed the code in decode_error_level().

--- pg_stat_monitor--2.3--2.4.sql
+++ pg_stat_monitor--2.3--2.4.sql
@@ -1,6 +1,29 @@
 -- complain if script is sourced in psql, rather than via CREATE EXTENSION
 \echo Use "ALTER EXTENSION pg_stat_monitor" to load this file. \quit
 
+-- Drop PostgreSQL 13 support from function
+CREATE OR REPLACE FUNCTION decode_error_level(elevel int)
+RETURNS text
+AS $$
+SELECT CASE
+    WHEN elevel = 0 THEN ''
+    WHEN elevel = 10 THEN 'DEBUG5'
+    WHEN elevel = 11 THEN 'DEBUG4'
+    WHEN elevel = 12 THEN 'DEBUG3'
+    WHEN elevel = 13 THEN 'DEBUG2'
+    WHEN elevel = 14 THEN 'DEBUG1'
+    WHEN elevel = 15 THEN 'LOG'
+    WHEN elevel = 16 THEN 'LOG_SERVER_ONLY'
+    WHEN elevel = 17 THEN 'INFO'
+    WHEN elevel = 18 THEN 'NOTICE'
+    WHEN elevel = 19 THEN 'WARNING'
+    WHEN elevel = 20 THEN 'WARNING_CLIENT_ONLY'
+    WHEN elevel = 21 THEN 'ERROR'
+    WHEN elevel = 22 THEN 'FATAL'
+    WHEN elevel = 23 THEN 'PANIC'
+END;
+$$ LANGUAGE SQL PARALLEL SAFE;
+
 DROP FUNCTION pgsm_create_13_view();
 DROP VIEW pg_stat_monitor;
 
