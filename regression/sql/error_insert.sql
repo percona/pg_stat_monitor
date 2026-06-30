3d4ef011365e6b49af75f39bc996b94df305f366
Andreas Karlsson <andreas.karlsson@percona.com>
2026-06-30 13:53:31 +0200

    Simplify the SQL a bit in our regression tests
    There is no need for OR REPLACE or IF EXISTS as we only support running
    on a clean database anyway.

--- regression/sql/error_insert.sql
+++ regression/sql/error_insert.sql
@@ -1,4 +1,3 @@
-DROP TABLE IF EXISTS company;
 CREATE TABLE company (
     id int PRIMARY KEY NOT NULL,
     name text NOT NULL
@@ -9,7 +8,7 @@ SELECT pg_stat_monitor_reset();
 INSERT INTO company (id, name) VALUES (1, 'Percona');
 INSERT INTO company (id, name) VALUES (1, 'Percona');
 
-DROP TABLE IF EXISTS company;
+DROP TABLE company;
 SELECT query, elevel, sqlcode, message FROM pg_stat_monitor ORDER BY query COLLATE "C", elevel;
 SELECT pg_stat_monitor_reset();
 DROP EXTENSION pg_stat_monitor;
