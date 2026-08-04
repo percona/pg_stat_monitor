4c434182f77f1728fb03f27d08ab0295b1a3dcde
Artem Gavrilov <artem.gavrilov@percona.com>
2026-08-04 15:24:54 +0200

    PG-2424 Backport test updates from PG19
    In PostgreSQL 19 some pg_stat_statements tests got updates. Backport
    them to pg_stat_monitor.

--- regression/sql/squashing.sql
+++ regression/sql/squashing.sql
@@ -300,6 +300,34 @@ SELECT WHERE '1' IN ('1'::int::text, '2'::int::text);
 SELECT WHERE '1' = ANY(array['1'::int::text, '2'::int::text]);
 SELECT query, calls FROM pg_stat_monitor ORDER BY query COLLATE "C";
 
+-- composite function with row expansion
+create table test_composite(x integer);
+CREATE FUNCTION composite_f(a integer[], out x integer, out y integer) returns
+record as $$            begin
+        x = a[1];
+        y = a[2];
+    end;
+$$ language plpgsql;
+SELECT pg_stat_monitor_reset() IS NOT NULL AS t;
+SELECT ((composite_f(array[1, 2]))).* FROM test_composite;
+SELECT ((composite_f(array[1, 2, 3]))).* FROM test_composite;
+SELECT ((composite_f(array[1, 2, 3]))).*, 1, 2, 3, ((composite_f(array[1, 2, 3]))).*, 1, 2
+FROM test_composite
+WHERE x IN (1, 2, 3);
+-- The \bind case from upstream is omitted here, see: https://perconadev.atlassian.net/browse/PG-1936
+-- ROW() expression with row expansion
+SELECT (ROW(ARRAY[1,2])).*;
+SELECT (ROW(ARRAY[1, 2], ARRAY[1, 2, 3])).*;
+SELECT 1, 2, (ROW(ARRAY[1, 2], ARRAY[1, 2, 3])).*, 3, 4;
+-- The \bind case from upstream is omitted here, see: https://perconadev.atlassian.net/browse/PG-1936
+SELECT query, calls FROM pg_stat_monitor ORDER BY query COLLATE "C";
+
+-- IN and ANY clauses with Vars are not squashed.
+SELECT * FROM test_squash a, test_squash b WHERE a.id IN (1, 2, 3, b.id, b.id + 1);
+SELECT * FROM test_squash a, test_squash b WHERE a.id = ANY (array[1, ((b.id + b.id * 2)), 5]);
+-- The \bind case from upstream is omitted here, see: https://perconadev.atlassian.net/browse/PG-1936
+SELECT query, calls FROM pg_stat_monitor ORDER BY query COLLATE "C";
+
 --
 -- cleanup
 --
@@ -309,5 +337,7 @@ DROP TABLE test_squash_numeric;
 DROP TABLE test_squash_bigint;
 DROP TABLE test_squash_cast CASCADE;
 DROP TABLE test_squash_jsonb;
+DROP TABLE test_composite;
+DROP FUNCTION composite_f;
 SELECT pg_stat_monitor_reset();
 DROP EXTENSION pg_stat_monitor;
