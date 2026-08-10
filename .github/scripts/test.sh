dfa40d34d711ae905892f245c7542da9bbd90abd
Andreas Karlsson <andreas.karlsson@percona.com>
2026-08-10 15:16:41 +0200

    Use /tmp as unix socket directory in the CI
    Since the Ubuntu packages uses /run/postgresql as the unix socket
    directory by default and that directory is owned by the postgres user we
    used to change owner of /run/postgresql but instead we can just use /tmp
    and simplify our setup.
    
    This way we do not even have to kill the default instance of PostgreSQL.

--- .github/scripts/test.sh
+++ .github/scripts/test.sh
@@ -5,11 +5,13 @@ set -e
 SCRIPT_DIR=$(cd -- "$(dirname "$0")" >/dev/null 2>&1; pwd -P)
 PG_BIN_DIR=$(pg_config --bindir)
 
-cd "$SCRIPT_DIR/../../"
+cd "$SCRIPT_DIR/../.."
+
+export PGHOST=/tmp
 
 # The two-phase transaction cases of the "utility" test need
 # max_prepared_transactions to be nonzero.
-OPTS='-c shared_preload_libraries=pg_stat_monitor -c max_prepared_transactions=5'
+OPTS="-k $PGHOST -c shared_preload_libraries=pg_stat_monitor -c max_prepared_transactions=5"
 
 if [ "$1" = sanitize ]; then
     OPTS+=' -c max_stack_depth=8MB'
