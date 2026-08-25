948bc3db03d1cee2f681cb5d2357c6f3198ffe8c
Artem Gavrilov <artem.gavrilov@percona.com>
2026-08-25 20:04:06 +0200

    PG-2624 Hide sensitive data for users with no priveleges
    pg_stat_monitor exposes some data that can be considered as sensitive,
    so apply the same priveleges mechanism as for query text.

--- regression/sql/privileges.sql
+++ regression/sql/privileges.sql
@@ -7,18 +7,46 @@
 CREATE EXTENSION pg_stat_monitor;
 SET pg_stat_monitor.pgsm_track_utility = off;
 SET pg_stat_monitor.pgsm_normalized_query = on;
+SET pg_stat_monitor.pgsm_track = 'all';
+SET pg_stat_monitor.pgsm_extract_comments = on;
+SET pg_stat_monitor.pgsm_enable_query_plan = on;
+
+CREATE FUNCTION regress_stats_nested(int) RETURNS int AS
+$$
+BEGIN
+    RETURN (SELECT $1 + 1);
+END
+$$ LANGUAGE plpgsql;
 
 CREATE ROLE regress_stats_superuser SUPERUSER;
 CREATE ROLE regress_stats_user1;
 CREATE ROLE regress_stats_user2;
+CREATE ROLE regress_stats_user3 NOINHERIT;
 GRANT pg_read_all_stats TO regress_stats_user2;
+GRANT pg_read_all_stats TO regress_stats_user3;
 
 SET ROLE regress_stats_superuser;
 SELECT pg_stat_monitor_reset() IS NOT NULL AS t;
 SELECT 1 AS "ONE";
+SELECT regress_stats_nested(1);
 
 SET ROLE regress_stats_user1;
 SELECT 1+1 AS "TWO";
+SELECT regress_stats_nested(2) /* comment */;
+SELECT 1 / 0;
+
+--
+-- regress_stats_user1 has no privileges to read the query text of queries
+-- executed by others but can see statistics like calls and rows.
+--
+
+SELECT r.rolname, ss.queryid <> 0 AS queryid_bool, ss.query, ss.top_query,
+       ss.message, ss.comments, ss.pgsm_query_id <> 0 AS pgsm_query_id_bool,
+       ss.planid <> 0 AS planid_bool,
+       ss.top_queryid <> 0 AS top_queryid_bool,
+       ss.client_ip IS NOT NULL AS client_ip_bool, ss.calls, ss.rows
+    FROM pg_stat_monitor ss JOIN pg_roles r ON ss.userid = r.oid
+    ORDER BY r.rolname, ss.query COLLATE "C", ss.calls, ss.rows;
 
 --
 -- A superuser can read all columns of queries executed by others,
@@ -26,17 +54,11 @@ SELECT 1+1 AS "TWO";
 --
 
 SET ROLE regress_stats_superuser;
-SELECT r.rolname, ss.queryid <> 0 AS queryid_bool, ss.query, ss.calls, ss.rows
-    FROM pg_stat_monitor ss JOIN pg_roles r ON ss.userid = r.oid
-    ORDER BY r.rolname, ss.query COLLATE "C", ss.calls, ss.rows;
-
---
--- regress_stats_user1 has no privileges to read the query text of queries
--- executed by others but can see statistics like calls and rows.
---
-
-SET ROLE regress_stats_user1;
-SELECT r.rolname, ss.queryid <> 0 AS queryid_bool, ss.query, ss.calls, ss.rows
+SELECT r.rolname, ss.queryid <> 0 AS queryid_bool, ss.query, ss.top_query,
+       ss.message, ss.comments, ss.pgsm_query_id <> 0 AS pgsm_query_id_bool,
+       ss.planid <> 0 AS planid_bool,
+       ss.top_queryid <> 0 AS top_queryid_bool,
+       ss.client_ip IS NOT NULL AS client_ip_bool, ss.calls, ss.rows
     FROM pg_stat_monitor ss JOIN pg_roles r ON ss.userid = r.oid
     ORDER BY r.rolname, ss.query COLLATE "C", ss.calls, ss.rows;
 
@@ -46,7 +68,34 @@ SELECT r.rolname, ss.queryid <> 0 AS queryid_bool, ss.query, ss.calls, ss.rows
 --
 
 SET ROLE regress_stats_user2;
-SELECT r.rolname, ss.queryid <> 0 AS queryid_bool, ss.query, ss.calls, ss.rows
+SELECT r.rolname, ss.queryid <> 0 AS queryid_bool, ss.query, ss.top_query,
+       ss.message, ss.comments, ss.pgsm_query_id <> 0 AS pgsm_query_id_bool,
+       ss.planid <> 0 AS planid_bool,
+       ss.top_queryid <> 0 AS top_queryid_bool,
+       ss.client_ip IS NOT NULL AS client_ip_bool, ss.calls, ss.rows
+    FROM pg_stat_monitor ss JOIN pg_roles r ON ss.userid = r.oid
+    ORDER BY r.rolname, ss.query COLLATE "C", ss.calls, ss.rows;
+
+--
+-- regress_stats_user3 is a member of pg_read_all_stats, but it is a NOINHERIT
+-- role, so the privilege does not apply until it does SET ROLE.
+--
+
+SET ROLE regress_stats_user3;
+SELECT r.rolname, ss.queryid <> 0 AS queryid_bool, ss.query, ss.top_query,
+       ss.message, ss.comments, ss.pgsm_query_id <> 0 AS pgsm_query_id_bool,
+       ss.planid <> 0 AS planid_bool,
+       ss.top_queryid <> 0 AS top_queryid_bool,
+       ss.client_ip IS NOT NULL AS client_ip_bool, ss.calls, ss.rows
+    FROM pg_stat_monitor ss JOIN pg_roles r ON ss.userid = r.oid
+    ORDER BY r.rolname, ss.query COLLATE "C", ss.calls, ss.rows;
+
+SET ROLE pg_read_all_stats;
+SELECT r.rolname, ss.queryid <> 0 AS queryid_bool, ss.query, ss.top_query,
+       ss.message, ss.comments, ss.pgsm_query_id <> 0 AS pgsm_query_id_bool,
+       ss.planid <> 0 AS planid_bool,
+       ss.top_queryid <> 0 AS top_queryid_bool,
+       ss.client_ip IS NOT NULL AS client_ip_bool, ss.calls, ss.rows
     FROM pg_stat_monitor ss JOIN pg_roles r ON ss.userid = r.oid
     ORDER BY r.rolname, ss.query COLLATE "C", ss.calls, ss.rows;
 
@@ -55,9 +104,12 @@ SELECT r.rolname, ss.queryid <> 0 AS queryid_bool, ss.query, ss.calls, ss.rows
 --
 
 RESET ROLE;
+DROP FUNCTION regress_stats_nested(int);
 DROP ROLE regress_stats_superuser;
 DROP ROLE regress_stats_user1;
 REVOKE pg_read_all_stats FROM regress_stats_user2;
 DROP ROLE regress_stats_user2;
+REVOKE pg_read_all_stats FROM regress_stats_user3;
+DROP ROLE regress_stats_user3;
 SELECT pg_stat_monitor_reset() IS NOT NULL AS t;
 DROP EXTENSION pg_stat_monitor;
